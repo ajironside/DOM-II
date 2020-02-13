97278244cddfbb7fcd777288f09cce153e41d438
Amanda Ironside <ironside.amanda@gmail.com>
2020-02-13 11:47:33 -0700

    mouseover, mouseleave, dblclick, stopPropagation, wheel, preventDefault added

--- js/index.js
+++ js/index.js
@@ -1,47 +1,86 @@
-//`mouseover`
+//`mouseover`  
+const signUp = document.querySelectorAll('.btn')
 
+const firstBtn = signUp[0]
+const secondBtn = signUp[1]
+const thirdBtn = signUp[2]
+
+firstBtn.addEventListener('mouseover', (event) => {
+    event.target.style.color = "purple";
+})
+
+secondBtn.addEventListener('mouseover', (event) => {
+    event.target.style.color = "orange";
+})
+
+thirdBtn.addEventListener('mouseover', (event) => {
+    event.target.style.color = "black";
+})
+
+// `blur`
+
+
+//`dblclick`, `stopPropagation`
+const bg = document.querySelector('body');
+const dest = document.querySelectorAll('.destination div');
+
+bg.addEventListener('dblclick', (event) => {
+    event.target.style.background = 'grey';
+    event.stopPropagation();
+})
+
+const twoClickEventHandler = (event) => {
+    event.target.style.background = 'teal';
+    event.stopPropagation();
+}
+
+dest[0].addEventListener('dblclick', twoClickEventHandler);
+dest[1].addEventListener('dblclick', twoClickEventHandler);
+dest[2].addEventListener('dblclick', twoClickEventHandler);
 
 //`keydown`
 
 
-//`wheel`
+//`wheel`, `preventDefault()`
+const topText = document.querySelector('.intro img')
+let scale = 1;
 
+topText.addEventListener('wheel', (event) => {
+    event.preventDefault();
 
-//`drag / drop`
+    scale += event.deltaY * -0.01;
 
+    scale = Math.min(Math.max(.125, scale), 4);
+
+    topText.style.transform = `scale(${scale})`;
+})
 
 //`load`
 
 
-//`focus`
-
 
 //`resize`
-const imgContent = document.querySelector('.img-content');
+const imgContent = document.querySelectorAll('.img-content');
 
-imgContent.addEventListener('mouseenter', () => {
-    imgContent.style.transform = "scale(1.3)";
-    imgContent.style.transform = "all 0.3s";
-});
+const enterEventHandler = (event) => {
+    event.target.style.transform = "scale(1.3)";
+    event.target.style.transform = "all 0.3s";
+}
 
-imgContent.addEventListener('mouseleave', () => {
-    imgContent.style.transform = "scale(1)";
-})
+imgContent[0].addEventListener('mouseenter', enterEventHandler);
+imgContent[1].addEventListener('mouseenter', enterEventHandler);
+
+//`mouseleave`
+const leaveEventHandler = (event) => {
+    event.target.style.transform = "scale(1)";
+}
+
+imgContent[0].addEventListener('mouseleave', leaveEventHandler);
+imgContent[1].addEventListener('mouseleave', leaveEventHandler);
 
 //`scroll`
 
 
-//`select`
+//'.content-pick'
 
-
-//`dblclick`
-const imgTop = document.querySelector('.intro img')
-
-imgTop.addEventListener('dblclick', () => {
-    imgTop.src = "img\ethan-robertson-SYx3UCHZJlo-unsplash.jpg";
-})
-
-//Nest two similar events somewhere in the site and prevent the event propagation properly
-
-
-//Stop the navigation items from refreshing the page by using `preventDefault()`
+//'.destination'
