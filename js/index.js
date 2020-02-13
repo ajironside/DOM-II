29fd165ddf64b99e7620200b477ae2c0661029ac
Amanda Ironside <ironside.amanda@gmail.com>
2020-02-13 10:13:35 -0700

    resize event added

--- js/index.js
+++ js/index.js
@@ -1,1 +1,47 @@
-// Your code goes here
+//`mouseover`
+
+
+//`keydown`
+
+
+//`wheel`
+
+
+//`drag / drop`
+
+
+//`load`
+
+
+//`focus`
+
+
+//`resize`
+const imgContent = document.querySelector('.img-content');
+
+imgContent.addEventListener('mouseenter', () => {
+    imgContent.style.transform = "scale(1.3)";
+    imgContent.style.transform = "all 0.3s";
+});
+
+imgContent.addEventListener('mouseleave', () => {
+    imgContent.style.transform = "scale(1)";
+})
+
+//`scroll`
+
+
+//`select`
+
+
+//`dblclick`
+const imgTop = document.querySelector('.intro img')
+
+imgTop.addEventListener('dblclick', () => {
+    imgTop.src = "img\ethan-robertson-SYx3UCHZJlo-unsplash.jpg";
+})
+
+//Nest two similar events somewhere in the site and prevent the event propagation properly
+
+
+//Stop the navigation items from refreshing the page by using `preventDefault()`
